e733f74923636ccc9ed77606c5409bfbd754ab02
James Shvarts <jshvarts@gmail.com>
2016-06-18 22:11:08 -0400

    added springboard

---
+++ app/src/main/java/com/jshvarts/flatstanley/activities/BrowseFlatStanleysActivity.java
@@ -0,0 +1,15 @@
+package com.jshvarts.flatstanley.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jshvarts.flatstanley.R;
+
+public class BrowseFlatStanleysActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.browse_flat_stanleys);
+    }
+}
---
+++ app/src/main/java/com/jshvarts/flatstanley/activities/ShareFlatStanleyActivity.java
@@ -0,0 +1,15 @@
+package com.jshvarts.flatstanley.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jshvarts.flatstanley.R;
+
+public class ShareFlatStanleyActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.share_flat_stanley);
+    }
+}
