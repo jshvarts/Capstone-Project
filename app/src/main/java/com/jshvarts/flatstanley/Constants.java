43eb9b555a163a8749b7131e7dfa537b6d340607
James Shvarts <james.shvarts@viacom.com>
2016-07-31 18:10:30 -0400

    store flat stanley to firebase

---
+++ app/src/main/java/com/jshvarts/flatstanley/Constants.java
@@ -0,0 +1,9 @@
+package com.jshvarts.flatstanley;
+
+public class Constants {
+    public static final String FIREBASE_URL = "https://flat-stanley.firebaseio.com";
+
+    public static final String IMAGE_DATA_FIELD = "imageData";
+    public static final String CAPTION_FIELD = "caption";
+    public static final String TIMESTAMP_FIELD = "timestamp";
+}
