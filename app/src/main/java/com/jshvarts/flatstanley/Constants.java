ca9cdc4bacffdba3be9910525ba23afad1b49410
James Shvarts <jshvarts@gmail.com>
2016-08-01 06:48:15 -0400

    finished share existing

--- app/src/main/java/com/jshvarts/flatstanley/Constants.java
+++ app/src/main/java/com/jshvarts/flatstanley/Constants.java
@@ -8,11 +8,11 @@ public class Constants {
     public static final String CAPTION_FIELD = "caption";
     public static final String TIMESTAMP_FIELD = "timestamp";
 
-    public static String getEntrytUri(Date date) {
-        if (date != null) {
-            return FIREBASE_URL + "/items/" + date.getTime();
-        } else {
-            return FIREBASE_URL + "/items";
-        }
+    public static String getEntrytUri(long time) {
+        return FIREBASE_URL + "/items/" + time;
+    }
+
+    public static String getEntrytUri() {
+        return FIREBASE_URL + "/items";
     }
 }
