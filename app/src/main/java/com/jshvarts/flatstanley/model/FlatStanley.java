c23a5804c3380de703f4e6d95d443c2a9dba58a5
James Shvarts <james.shvarts@viacom.com>
2016-07-31 23:59:25 -0400

    added share existing

--- app/src/main/java/com/jshvarts/flatstanley/model/FlatStanley.java
+++ app/src/main/java/com/jshvarts/flatstanley/model/FlatStanley.java
@@ -1,27 +1,53 @@
 package com.jshvarts.flatstanley.model;
 
-import android.graphics.Bitmap;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class FlatStanley {
-    private final Bitmap bitmap;
-    private final String caption;
-    private final String timestamp;
+    private String id;
+    private String imageData;
+    private String caption;
+    private String timestamp;
 
-    public FlatStanley(Bitmap bitmap, String caption, String timestamp) {
-        this.bitmap = bitmap;
+    public FlatStanley() {
+        // dummy constructor to enable Jackson deserialization by Firebase
+    }
+
+    public FlatStanley(String imageData, String caption, String timestamp) {
+        this.imageData = imageData;
         this.caption = caption;
         this.timestamp = timestamp;
     }
 
-    public Bitmap getBitmap() {
-        return bitmap;
+    public String getId() {
+        return id;
     }
 
     public String getCaption() {
         return caption;
     }
 
+    public String getImageData() {
+        return imageData;
+    }
+
     public String getTimestamp() {
         return timestamp;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public void setImageData(String imageData) {
+        this.imageData = imageData;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
     }
 }
