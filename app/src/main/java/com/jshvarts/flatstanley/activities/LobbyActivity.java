0093ab9f97020228d18ce6c8bc4aea7cc265691d
James Shvarts <james.shvarts@viacom.com>
2016-08-08 20:11:58 -0400

    added another google play service: ads

--- app/src/main/java/com/jshvarts/flatstanley/activities/LobbyActivity.java
+++ app/src/main/java/com/jshvarts/flatstanley/activities/LobbyActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.jshvarts.flatstanley.R;
 
 import butterknife.BindView;
@@ -19,6 +21,9 @@ public class LobbyActivity extends AppCompatActivity {
     @BindView(R.id.browse)
     protected Button browseButton;
 
+    @BindView(R.id.adView)
+    protected AdView adView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +31,14 @@ public class LobbyActivity extends AppCompatActivity {
         setContentView(R.layout.lobby);
 
         ButterKnife.bind(this);
+
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        adView.loadAd(adRequest);
     }
 
     @OnClick(R.id.takePic)
